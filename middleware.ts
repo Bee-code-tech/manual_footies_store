4f0937f2eeed13e8bb87aeda1373956a00caf4c7
Bee-code-tech <babawaleolatunji64@gmail.com>
2023-10-22 16:51:16 +0100

    public api routing

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({});
+export default authMiddleware({
+  // "/" will be accessible to all users
+  publicRoutes: ["/"]
+});
+ 
  
 export const config = {
       matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
